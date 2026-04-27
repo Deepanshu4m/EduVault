508be785a85167ea949a58273bdc43df0c60dbc6
Deepanshu4m <deepanshubisht2703@gmail.com>
2026-04-27 19:21:27 +0530

    refactor: improve code formatting and consistency in Catalog, CourseDetails, and ViewCourse components

--- eduvault/src/pages/ViewCourse.jsx
+++ eduvault/src/pages/ViewCourse.jsx
@@ -8,38 +8,39 @@ import CourseReviewModal from '../components/core/ViewCourse/CourseReviewModal';
 
 const ViewCourse = () => {
 
-    const [reviewModal, setReviewModal] = useState(false);
-    const {courseId} = useParams();
-    const {token} = useSelector((state)=>state.auth);
-    const dispatch = useDispatch();
+  const [reviewModal, setReviewModal] = useState(false);
+  const { courseId } = useParams();
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-    useEffect(()=> {
-        const setCourseSpecificDetails = async() => {
-              const courseData = await getFullDetailsOfCourse(courseId, token);
-              dispatch(setCourseSectionData(courseData.courseDetails.courseContent));
-              dispatch(setEntireCourseData(courseData.courseDetails));
-              dispatch(setCompletedLectures(courseData.completedVideos));
-              let lectures = 0;
-              courseData?.courseDetails?.courseContent?.forEach((sec) => {
-                lectures += sec.subSection.length
-              })  
-              dispatch(setTotalNoOfLectures(lectures));
-        }
-        setCourseSpecificDetails();
-    },[]);
+  useEffect(() => {
+    const setCourseSpecificDetails = async () => {
+      const courseData = await getFullDetailsOfCourse(courseId, token);
+      dispatch(setCourseSectionData(courseData.courseDetails.courseContent));
+      dispatch(setEntireCourseData(courseData.courseDetails));
+      dispatch(setCompletedLectures(courseData.completedVideos));
+      let lectures = 0;
+      courseData?.courseDetails?.courseContent?.forEach((sec) => {
+        lectures += sec.subSection.length
+      })
+      dispatch(setTotalNoOfLectures(lectures));
+    }
+    setCourseSpecificDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
 
   return (
     <>
-        <div className="relative flex min-h-[calc(100vh-3.5rem)]">
-            <VideoDetailsSidebar setReviewModal={setReviewModal} />
-            <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
-              <div className="mx-6">
-                <Outlet />
-              </div>
-            </div>
+      <div className="relative flex min-h-[calc(100vh-3.5rem)]">
+        <VideoDetailsSidebar setReviewModal={setReviewModal} />
+        <div className="h-[calc(100vh-3.5rem)] flex-1 overflow-auto">
+          <div className="mx-6">
+            <Outlet />
+          </div>
         </div>
-        {reviewModal && <CourseReviewModal setReviewModal={setReviewModal} />}
+      </div>
+      {reviewModal && <CourseReviewModal setReviewModal={setReviewModal} />}
     </>
   )
 }
