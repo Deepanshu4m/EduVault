358d93a85aa5690c3728ef091c9321969d09818a
Deepanshu4m <deepanshubisht2703@gmail.com>
2026-04-27 18:56:24 +0530

    fix vercel build - disable eslint

--- eduvault/server/utils/mailSender.js
+++ eduvault/server/utils/mailSender.js
@@ -2,13 +2,15 @@ const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
     try {
-        let transporter = nodemailer?.createTransport({
-            service: process.env.MAIL_HOST,
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            port: 587,
+            family: 4,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
-            },
-        });
+            }
+        })
 
         let info = await transporter?.sendMail({
             from: "EduVault || Best E-Learning Platform - by Deepanshu",
